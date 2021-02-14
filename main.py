df082158ab2c71f18c5d655dd9be06946da8723b
Leo976 <leocraft.lemoosy@gmail.com>
2021-02-14 14:02:40 +0100

    Ajout de la fonction print_matrix

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+matrix = [[0 for j in range(7)] for k in range(6)]
+
+
+
+
+
+def print_matrix():
+
+	for line in matrix:
+		print(*line, sep = '')
+
+
+
+
+print_matrix()
+
+
+
+input()
