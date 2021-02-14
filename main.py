019d48ffda3d1563a594be291aa947d408a2ef8a
Leo976 <leocraft.lemoosy@gmail.com>
2021-02-14 14:10:26 +0100

    Ajout des fonctions get_lines & get_columns

--- main.py
+++ main.py
@@ -9,6 +9,10 @@
 matrix = [[0 for j in range(7)] for k in range(6)]
 
 
+matrix[0][2] = 1
+matrix[0][3] = 1
+matrix[0][4] = 1
+
 
 
 
@@ -17,10 +21,18 @@ def print_matrix():
 	for line in matrix:
 		print(*line, sep = '')
 
+def get_lines():
+
+	return matrix
+
+def get_columns():
+
+	return list(map(list, zip(*matrix)))
+		
 
 
 
-print_matrix()
+
 
 
 
