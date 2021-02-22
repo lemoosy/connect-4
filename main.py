ce85a17cac5b0f72aaf8945c5b92ac6284ef4b4d
Leo976 <leocraft.lemoosy@gmail.com>
2021-02-22 18:21:13 +0100

    Affichage du fond

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ pygame.init()
 window = pygame.display.set_mode((SIZE_WINDOW_X, SIZE_WINDOW_Y))
 pygame.display.set_caption('Connect 4')
 
+images = dict()
+images['background.png'] = pygame.image.load('img/background.png')
+images['red_piece.png'] = pygame.image.load('img/red_piece.png')
+images['yellow_piece.png'] = pygame.image.load('img/yellow_piece.png')
 
+window.blit(images['background.png'], (0,0))
+pygame.display.flip()
 
 
 input()
